06619b59d4c0b2f02e5017e63311733dc6a8c72e
iossocket <avx302@gmail.com>
2024-07-03 13:58:42 +0800

    add entry point to access tiny react

---
+++ packages/react-dom/src/client/ReactDOMRoot.ts
@@ -0,0 +1,26 @@
+import type { ReactNodeList } from "shared/ReactTypes";
+import { createFiberRoot } from "react-reconciler/src/ReactFiberRoot";
+import type { FiberRoot, Container } from "react-reconciler/src/ReactInternalTypes";
+import { updateContainer } from "react-reconciler/src/ReactFiberReconciler";
+
+type RootType = {
+  render: (children: ReactNodeList) => void,
+  _internalRoot: FiberRoot,
+}
+
+function ReactDOMRoot(_internalRoot: FiberRoot) {
+  this._internalRoot = _internalRoot;
+}
+
+ReactDOMRoot.prototype.render = function (children: ReactNodeList) {
+  updateContainer(children, this._internalRoot);
+}
+
+export const createRoot = (container: Container): RootType => {
+  const root: FiberRoot = createFiberRoot(container);
+  return new ReactDOMRoot(root);
+};
+
+export default {
+  createRoot
+}
---
+++ packages/react-reconciler/src/ReactFiberReconciler.ts
@@ -0,0 +1,10 @@
+import { ReactNodeList } from "shared/ReactTypes";
+import { FiberRoot } from "./ReactInternalTypes";
+
+export function updateContainer(element: ReactNodeList, container: FiberRoot) {
+  const current = container.current;
+  current.memoizedState = { element };
+
+
+  console.log('%c [  ]-9', 'font-size:13px; background:pink; color:#bf2c9f;', current)
+}
